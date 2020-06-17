39da1904f222137bc3f8b99f1ab68c98c195a587
‘peter-robert-cole’ <petercole3@gmail.com>
2020-06-16 23:01:26 -0700

    removed excess tags

--- src/Components/Movie/index.js
+++ src/Components/Movie/index.js
@@ -5,17 +5,15 @@ export default class Movie extends Component {
 	render() {
 		const { title, genre, stock, rate } = this.props;
 		return (
-			<React.Fragment>
-				<tr>
-					<td>{title}</td>
-					<td>{genre}</td>
-					<td>{stock}</td>
-					<td>{rate}</td>
-					<td>
-						<button>Delete</button>
-					</td>
-				</tr>
-			</React.Fragment>
+			<tr>
+				<td>{title}</td>
+				<td>{genre}</td>
+				<td>{stock}</td>
+				<td>{rate}</td>
+				<td>
+					<button>Delete</button>
+				</td>
+			</tr>
 		);
 	}
 }
