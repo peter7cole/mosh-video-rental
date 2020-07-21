d62a1072e3cf6678ddf37ee917aa7e37dd74b57d
‘peter-robert-cole’ <petercole3@gmail.com>
2020-07-21 16:10:28 -0700

    beginnings of Like component

--- src/Components/MovieTable/index.js
+++ src/Components/MovieTable/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './MovieTable.css';
+import LikeHeart from '../common/LikeHeart';
 
 export default class Table extends Component {
 	render() {
@@ -13,18 +14,22 @@ export default class Table extends Component {
 							<th>Stock</th>
 							<th>Rate</th>
 							<th></th>
+							<th></th>
 						</tr>
 					</thead>
 					<tbody>
-						{this.props.movies.map(movie => (
-							<tr key={movie._id}>
-								<td>{movie.genre.name}</td>
-								<td>{movie.title}</td>
-								<td>{movie.numberInStock}</td>
-								<td>{movie.dailyRentalRate}</td>
+						{this.props.movies.map(m => (
+							<tr key={m._id}>
+								<td>{m.genre.name}</td>
+								<td>{m.title}</td>
+								<td>{m.numberInStock}</td>
+								<td>{m.dailyRentalRate}</td>
+								<td>
+									<LikeHeart liked={m.liked} />
+								</td>
 								<td>
 									<button
-										onClick={() => this.props.onDelete(movie)}
+										onClick={() => this.props.onDelete(m)}
 										className="btn btn-danger btn-sm"
 									>
 										Delete
---
+++ src/Components/common/LikeHeart/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class LikeHeart extends Component {
+	render() {
+		return (
+			<div>
+				<i className="fas fa-heart-o" aria-hidden="true"></i>
+			</div>
+		);
+	}
+}
