ec4c964b7716f282787083cc4642b7a63487f2a1
‘peter-robert-cole’ <petercole3@gmail.com>
2020-07-25 15:00:00 -0700

    selectable genre menu, no filter yet

--- src/Components/App/index.js
+++ src/Components/App/index.js
@@ -3,19 +3,23 @@ import './App.css';
 import Counter from '../MovieCounter';
 import Table from '../MovieTable';
 import { getMovies } from '../../services/fakeMovieService';
-import { genres } from '../../services/fakeGenreService';
+import { getGenres } from '../../services/fakeGenreService';
 import Pagination from '../common/Pagination';
 import paginate from '../../utils/paginate';
 import ListGroup from '../common/ListGroup';
 
 export default class App extends Component {
 	state = {
-		movies: getMovies(),
-		movieCount: getMovies().length,
-		genres: genres,
+		movies: [],
+		genres: [],
 		itemsPerPage: 4,
 		currentPage: 1
 	};
+
+	// this method is called when an instance of this component is rendered in the DOM
+	componentDidMount() {
+		this.setState({ movies: getMovies(), genres: getGenres() });
+	}
 
 	handleLike = movie => {
 		const movies = [...this.state.movies];
@@ -35,32 +39,45 @@ export default class App extends Component {
 		this.setState({ currentPage: page });
 	};
 
+	handleGenreSelect = genre => {
+		this.setState({ selectedGenre: genre });
+	};
+
 	render() {
 		const {
 			movies,
-			movieCount,
 			genres,
 			itemsPerPage,
-			currentPage
+			currentPage,
+			selectedGenre
 		} = this.state;
+		const movieCount = movies.length;
 		if (movieCount === 0) return <p>There are no movies in the database</p>;
 
 		return (
-			<main className="container">
-				<ListGroup genres={genres} />
-				<Counter movieCount={movieCount} />
-				<Table
-					moviesPaginated={paginate(movies, currentPage, itemsPerPage)}
-					onLike={this.handleLike}
-					onDelete={this.handleDelete}
-				/>
-				<Pagination
-					itemsCount={movieCount}
-					itemsPerPage={itemsPerPage}
-					onPageChange={this.handlePageChange}
-					currentPage={currentPage}
-				/>
-			</main>
+			<div className="row">
+				<div className="col-2">
+					<ListGroup
+						items={genres}
+						onItemSelect={this.handleGenreSelect}
+						selectedItem={selectedGenre}
+					/>
+				</div>
+				<div className="col">
+					<Counter movieCount={movieCount} />
+					<Table
+						moviesPaginated={paginate(movies, currentPage, itemsPerPage)}
+						onLike={this.handleLike}
+						onDelete={this.handleDelete}
+					/>
+					<Pagination
+						itemsCount={movieCount}
+						itemsPerPage={itemsPerPage}
+						onPageChange={this.handlePageChange}
+						currentPage={currentPage}
+					/>
+				</div>
+			</div>
 		);
 	}
 }
--- src/Components/common/ListGroup/index.js
+++ src/Components/common/ListGroup/index.js
@@ -1,17 +1,37 @@
 import React from 'react';
 
 const ListGroup = props => {
-	const { genres } = props;
+	const {
+		items,
+		valueProperty,
+		textProperty,
+		onItemSelect,
+		selectedItem
+	} = props;
 	return (
 		<ul className="list-group">
-			<li className="list-group-item">All Genres</li>
-			{genres.map(genre => (
-				<li key={genre._id} className="list-group-item">
-					{genre.name}
+			{items.map(item => (
+				<li
+					key={item[valueProperty]}
+					className={
+						item === selectedItem ? 'list-group-item active' : 'list-group-item'
+					}
+					onClick={() => onItemSelect(item)}
+				>
+					{item[textProperty]}
 				</li>
 			))}
 		</ul>
 	);
 };
+
+// Use bracket notation in the list element above to access properties dynamically instead of dot notation. Allows to be reused for any kind of lists in the future.
+
+ListGroup.defaultProps = {
+	textProperty: 'name',
+	valueProperty: '_id'
+};
+
+// Default props above are used to clean up the component props in the parent component.
 
 export default ListGroup;
