e3abc826dae755b30f23edd34237ac6c0224bbaf
‘peter-robert-cole’ <petercole3@gmail.com>
2021-04-28 11:16:30 -0700

    Updates

---
+++ precache-manifest.ab691165ea6c6ef2e8d3ad5d7d572d14.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9ceef798e324f63d561b7b943f922e1",
+    "url": "/mosh-video-rental/index.html"
+  },
+  {
+    "revision": "69be4c6a48f81a458694",
+    "url": "/mosh-video-rental/static/css/2.cf422453.chunk.css"
+  },
+  {
+    "revision": "da4aaadbf019d4c921cb",
+    "url": "/mosh-video-rental/static/css/main.7a83bddc.chunk.css"
+  },
+  {
+    "revision": "69be4c6a48f81a458694",
+    "url": "/mosh-video-rental/static/js/2.53c1fb0c.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/mosh-video-rental/static/js/2.53c1fb0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da4aaadbf019d4c921cb",
+    "url": "/mosh-video-rental/static/js/main.e0692762.chunk.js"
+  },
+  {
+    "revision": "fbc5583d1c1524c82ed3",
+    "url": "/mosh-video-rental/static/js/runtime-main.a282b60c.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/mosh-video-rental/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/mosh-video-rental/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/mosh-video-rental/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/mosh-video-rental/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/mosh-video-rental/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
